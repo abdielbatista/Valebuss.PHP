372bebfa36d9dd6700b5db583cc718838e430f25
gabriel_de_souza_miranda <gabasan12@gmail.com>
2021-04-18 21:55:41 -0300

    atualizando index

--- app/Views/index_login.php
+++ app/Views/index_login.php
@@ -123,38 +123,37 @@ echo $this->include('includes/footer');
                 </div>
             </div>
 
-<!-- card -->
-<div class="card" style="width: 25rem;">
-    <?php if (isset($viagens)) : ?>
-    <?php foreach ($viagens as $v) : ?>
+            <!-- card style="width: 25rem;" -->
+            <div class="container " >
+                <div class="row">
+                    <?php if (isset($viagens)) : ?>
+                    <?php foreach ($viagens as $v) : ?>
 
-        <!--if (foto de perfil for "none" coloca uma foto padrao) -->
-        <div class="card-body">
-          <h5 class="card-title">Carona para <?php echo $v->end_destino ?></h5>
-          <p class="card-text">Endereço de saida: <?php echo $v->end_origem ?></p>
-          <p class="card-text">Cidade origem: <?php echo $v->cidade_origem ?></p>
-          <p class="card-text">Cidade destino: <?php echo $v->cidade_destino ?></p>
-          <p class="card-text">Hora: <?php echo $v->horario_saida ?></p>
-          <p class="card-text">Descrição: <?php echo $v->descricao ?></p>
-          <p class="card-text">Publicado por: <?php echo $v->cod_usuario ?></p>
+                        <!--if (foto de perfil for "none" coloca uma foto padrao) -->
+                        <div class="card-body border" style="width:20rem">
+                            <h5 class="card-title">Carona para <?php echo $v->end_destino ?></h5>
+                            <p class="card-text">Endereço de saida: <?php echo $v->end_origem ?></p>
+                            <p class="card-text">Cidade origem: <?php echo $v->cidade_origem ?></p>
+                            <p class="card-text">Cidade destino: <?php echo $v->cidade_destino ?></p>
+                            <p class="card-text">Hora: <?php echo $v->horario_saida ?></p>
+                            <p class="card-text">Descrição: <?php echo $v->descricao ?></p>
+                            <p class="card-text">Publicado por: <?php echo $v->cod_usuario ?></p>
 
-          <form id="inline" method="post" action="<?php echo base_url("viagens/aceitar") ?>">
-            <?php $email = $_SESSION['email']; ?>
-            <input hidden type="text" class="form-control" id="emailuser" name="emailuser" value="<?php echo $email ?>">
-            <label hidden for="codviagem"><?php echo $v->cod_viagem ?></label>
-            <input hidden type="text" class="form-control" id="codviagem" name="codviagem" value="<?php echo $v->cod_viagem ?>">
+                            <form id="inline" method="post" action="<?php echo base_url("viagens/aceitar") ?>">
+                                <?php $email = $_SESSION['email']; ?>
+                                <input hidden type="text" class="form-control" id="emailuser" name="emailuser" value="<?php echo $email ?>">
+                                <label hidden for="codviagem"><?php echo $v->cod_viagem ?></label>
+                                <input hidden type="text" class="form-control" id="codviagem" name="codviagem" value="<?php echo $v->cod_viagem ?>">
 
 
-            <button type="submit" class="btn btn-primary">Aceitar Viajem</button>
-          </form>
-        </div>
-         <!-- -->
-        <?php endforeach ?>
-        <?php endif ?>
-</div>
-     
-      
-
+                                <button type="submit" class="btn btn-primary">Aceitar Viajem</button>
+                            </form>
+                        </div>
+                        <!-- -->
+                        <?php endforeach ?>
+                        <?php endif ?>
+                </div>    
+            </div>
             
         </div>
     </div>
